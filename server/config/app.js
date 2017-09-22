de3a69e8799845011da116b9202d613834ff5a23
randleignacio <randle29@ufl.edu>
2017-09-22 19:09:48 -0400

    changed files

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,13 @@
-var config = require('./config'), 
-    mongoose = require('mongoose'),   
+
+var config = require('./config'),
+    mongoose = require('mongoose'),
     express = require('./express');
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+
+app.listen(process.env.PORT || 8080, function() {
+    //console.log('App listening on port', config.port);
+    console.log("Express server listening on port %d in %s mode," this.address().port, app.settings.env);
   });
 };
