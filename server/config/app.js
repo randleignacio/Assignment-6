7fb3aa9a07c748379089d3679c405d7b12517557
randleignacio <randle29@ufl.edu>
2017-09-22 19:16:49 -0400

    git ignore

--- server/config/app.js
+++ server/config/app.js
@@ -6,7 +6,7 @@ var config = require('./config'),
 module.exports.start = function() {
   var app = express.init();
 
-app.listen(process.env.PORT || 8080, function() {
+app.listen(process.env.PORT || config.port, function() {
     //console.log('App listening on port', config.port);
     console.log("Express server listening on port %d in %s mode," this.address().port, app.settings.env);
   });
